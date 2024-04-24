20bf50f7e4e7876481be02afcc657029804023f3
holus-bolus <ustinovoleksij@gmail.com>
2024-04-24 22:31:46 +0300

    Added the validation for the review

--- src/validation/services/review.js
+++ src/validation/services/review.js
@@ -1,8 +1,28 @@
+const Review = require('~/models/review')
+
 const allowedReviewFieldsForUpdate = {
   comment: true,
   rating: true
 }
 
+const isAuthor = async (req, res, next) => {
+  const { id: userId } = req.user
+  const { id: reviewId } = req.params
+
+  const review = await Review.findById(reviewId)
+
+  if (!review) {
+    return res.status(404).json({ message: 'Review not found' })
+  }
+
+  if (review.author.toString() !== userId) {
+    return res.status(403).json({ message: 'You do not have permission to perform this action' })
+  }
+
+  next()
+}
+
 module.exports = {
-  allowedReviewFieldsForUpdate
+  allowedReviewFieldsForUpdate,
+  isAuthor
 }
