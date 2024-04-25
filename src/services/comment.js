be6890fb2559ded2f2f6bc7c83bfdca47d6ce2c0
holus-bolus <ustinovoleksij@gmail.com>
2024-04-25 21:03:30 +0300

    Redactored the code

--- src/services/comment.js
+++ src/services/comment.js
@@ -3,37 +3,19 @@ const Comment = require('~/models/comment')
 const commentService = {
   addComment: async (data) => {
     const { text, author, authorRole, cooperationId } = data
-    const commentData = {
+    const comment = await Comment.create({
       author,
       text,
       cooperation: cooperationId,
       authorRole
-    }
-    let comment = new Comment(commentData)
-    await comment.save()
-    comment = await Comment.findById(comment._id).populate({
+    })
+    return await Comment.findById(comment._id).select('-authorRole').populate({
       path: 'author',
       select: 'firstName lastName'
     })
-
-    const result = {
-      _id: comment._id,
-      author: {
-        _id: comment.author._id,
-        firstName: comment.author.firstName,
-        lastName: comment.author.lastName
-      },
-      text: comment.text,
-      cooperation: comment.cooperation,
-      createdAt: comment.createdAt,
-      updatedAt: comment.updatedAt
-    }
-
-    return result
   },
-
   getComments: async (cooperationId) => {
-    return await Comment.find({ cooperation: cooperationId }).populate({
+    return await Comment.find({ cooperation: cooperationId }).select('-authorRole').populate({
       path: 'author',
       select: 'firstName lastName'
     })
