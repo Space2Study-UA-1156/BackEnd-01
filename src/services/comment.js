81f2706297aeb08e19db3b5acd15a92e63ac7de7
holus-bolus <ustinovoleksij@gmail.com>
2024-04-25 12:11:53 +0300

    Created the separate function to add comments

--- src/services/comment.js
+++ src/services/comment.js
@@ -3,12 +3,40 @@ const Comment = require('~/models/comment')
 const commentService = {
   addComment: async (data) => {
     const { text, author, authorRole, cooperationId } = data
+    const commentData = {
+      author,
+      text,
+      cooperation: cooperationId,
+      authorRole
+    }
+    let comment = new Comment(commentData)
+    await comment.save()
+    comment = await Comment.findById(comment._id).populate({
+      path: 'author',
+      select: 'firstName lastName'
+    })
 
-    return await Comment.create({ author, cooperation: cooperationId, text, authorRole })
+    const result = {
+      _id: comment._id,
+      author: {
+        _id: comment.author._id,
+        firstName: comment.author.firstName,
+        lastName: comment.author.lastName
+      },
+      text: comment.text,
+      cooperation: comment.cooperation,
+      createdAt: comment.createdAt,
+      updatedAt: comment.updatedAt
+    }
+
+    return result
   },
 
-  getComments: async (cooperationId, userId) => {
-    return await Comment.find({ cooperation: cooperationId, author: userId }).exec()
+  getComments: async (cooperationId) => {
+    return await Comment.find({ cooperation: cooperationId }).populate({
+      path: 'author',
+      select: 'firstName lastName'
+    })
   }
 }
 
