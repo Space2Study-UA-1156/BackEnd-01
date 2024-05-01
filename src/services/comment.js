5092fc3f2c608eded9bab553e02a2ae3c389ab94
Vitalii Voloshyn <56509285+voloshkin-v@users.noreply.github.com>
2024-05-01 18:42:26 +0300

    respond with 403 if user is not involed in a cooperation (#56)

--- src/services/comment.js
+++ src/services/comment.js
@@ -1,8 +1,16 @@
 const Comment = require('~/models/comment')
+const Cooperation = require('~/models/cooperation')
+const { createForbiddenError } = require('~/utils/errorsHelper')
 
 const commentService = {
   addComment: async (data) => {
     const { text, author, authorRole, cooperationId } = data
+
+    const cooperation = await Cooperation.findById(cooperationId)
+    if (cooperation.initiator.toString() !== author && cooperation.receiver.toString() !== author) {
+      throw createForbiddenError()
+    }
+
     const comment = await Comment.create({
       author,
       text,
@@ -14,7 +22,12 @@ const commentService = {
       select: 'firstName lastName'
     })
   },
-  getComments: async (cooperationId) => {
+  getComments: async (cooperationId, userId) => {
+    const cooperation = await Cooperation.findById(cooperationId)
+    if (cooperation.initiator.toString() !== userId && cooperation.receiver.toString() !== userId) {
+      throw createForbiddenError()
+    }
+
     return await Comment.find({ cooperation: cooperationId }).select('-authorRole').populate({
       path: 'author',
       select: 'firstName lastName'
