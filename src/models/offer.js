40eb646c7102db6579636e8f2ecb7678bb761d1d
YaroslavLys <74725159+YaroslavLys@users.noreply.github.com>
2024-05-14 22:25:55 +0300

    Added offer model validation

--- src/models/offer.js
+++ src/models/offer.js
@@ -2,60 +2,88 @@ const { Schema, model } = require('mongoose')
 const Category = require('~/models/category')
 const Subject = require('~/models/subject')
 const {
-  enums: { MAIN_ROLE_ENUM, SPOKEN_LANG_ENUM, PROFICIENCY_LEVEL_ENUM, OFFER_STATUS_ENUM }
+  enums: {
+    MAIN_ROLE_ENUM,
+    SPOKEN_LANG_ENUM,
+    PROFICIENCY_LEVEL_ENUM,
+    OFFER_STATUS_ENUM
+  }
 } = require('~/consts/validation')
 const { USER, SUBJECT, CATEGORY, OFFER } = require('~/consts/models')
-const { ENUM_CAN_BE_ONE_OF } = require('~/consts/errors')
+const {
+  ENUM_CAN_BE_ONE_OF,
+  FIELD_CANNOT_BE_EMPTY,
+  VALUE_MUST_BE_ABOVE,
+  FIELD_CANNOT_BE_SHORTER,
+  FIELD_CANNOT_BE_LONGER,
+  FIELD_MUST_BE_SELECTED
+} = require('~/consts/errors')
 
 const offerSchema = new Schema(
   {
     price: {
-      type: Number
+      type: Number,
+      required: [true, FIELD_CANNOT_BE_EMPTY('price')],
+      min: [1, VALUE_MUST_BE_ABOVE('price', 1)]
     },
     proficiencyLevel: {
       type: [String],
       enum: {
         values: PROFICIENCY_LEVEL_ENUM,
-        message: ENUM_CAN_BE_ONE_OF('proficiency level', PROFICIENCY_LEVEL_ENUM)
-      }
+        message: ENUM_CAN_BE_ONE_OF('proficiencyLevel', PROFICIENCY_LEVEL_ENUM)
+      },
+      required: [true, FIELD_CANNOT_BE_EMPTY('proficiencyLevel')]
     },
     title: {
-      type: String
+      type: String,
+      minLength: [1, FIELD_CANNOT_BE_SHORTER('title', 1)],
+      maxLength: [100, FIELD_CANNOT_BE_LONGER('title', 100)],
+      required: [true, FIELD_CANNOT_BE_EMPTY('title')],
+      trim: true
     },
     description: {
-      type: String
+      type: String,
+      minLength: [1, FIELD_CANNOT_BE_SHORTER('description', 1)],
+      maxLength: [1000, FIELD_CANNOT_BE_LONGER('description', 1000)],
+      required: [true, FIELD_CANNOT_BE_EMPTY('description')],
+      trim: true
     },
     languages: {
       type: [String],
       enum: {
         values: SPOKEN_LANG_ENUM,
         message: ENUM_CAN_BE_ONE_OF('language', SPOKEN_LANG_ENUM)
-      }
+      },
+      required: [true, FIELD_MUST_BE_SELECTED('language')]
     },
     authorRole: {
       type: String,
       enum: {
         values: MAIN_ROLE_ENUM,
-        message: ENUM_CAN_BE_ONE_OF('author role', MAIN_ROLE_ENUM)
+        message: ENUM_CAN_BE_ONE_OF('authorRole', MAIN_ROLE_ENUM),
+        required: [true, FIELD_MUST_BE_SELECTED('authorRole')]
       }
     },
     author: {
       type: Schema.Types.ObjectId,
-      ref: USER
+      ref: USER,
+      required: true
     },
     subject: {
       type: Schema.Types.ObjectId,
-      ref: SUBJECT
+      ref: SUBJECT,
+      required: true
     },
     category: {
       type: Schema.Types.ObjectId,
-      ref: CATEGORY
+      ref: CATEGORY,
+      required: true
     },
     status: {
       type: String,
       enum: {
         values: OFFER_STATUS_ENUM,
-        message: ENUM_CAN_BE_ONE_OF('offer status', OFFER_STATUS_ENUM)
+        message: ENUM_CAN_BE_ONE_OF('status', OFFER_STATUS_ENUM)
       },
       default: OFFER_STATUS_ENUM[0]
     },
@@ -63,10 +91,14 @@ const offerSchema = new Schema(
       type: [
         {
           question: {
-            type: String
+            type: String,
+            required: [true, FIELD_CANNOT_BE_EMPTY('question')],
+            trim: true
           },
           answer: {
-            type: String
+            type: String,
+            required: [true, FIELD_CANNOT_BE_EMPTY('answer')],
+            trim: true
           }
         }
       ]
@@ -81,18 +113,18 @@ const offerSchema = new Schema(
   }
 )
 
-offerSchema.statics.calcTotalOffers = async function (category, subject, authorRole) {
+offerSchema.statics.calcTotalOffers = async function(category, subject, authorRole) {
   const categoryTotalOffersQty = await this.countDocuments({ category, authorRole })
   await Category.findByIdAndUpdate(category, { $set: { [`totalOffers.${authorRole}`]: categoryTotalOffersQty } }).exec()
   const subjectTotalOffersQty = await this.countDocuments({ subject, authorRole })
   await Subject.findByIdAndUpdate(subject, { $set: { [`totalOffers.${authorRole}`]: subjectTotalOffersQty } }).exec()
 }
 
-offerSchema.post('save', async function (doc) {
+offerSchema.post('save', async function(doc) {
   doc.constructor.calcTotalOffers(doc.category, doc.subject, doc.authorRole)
 })
 
-offerSchema.post('findOneAndRemove', async function (doc) {
+offerSchema.post('findOneAndRemove', async function(doc) {
   doc.constructor.calcTotalOffers(doc.category, doc.subject, doc.authorRole)
 })
 
